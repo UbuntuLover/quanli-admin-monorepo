fa17eef2c0a27ad16ba88d5a896ee055ace10e7c
xuetao <xuetao2015test@gmail.com>
2026-05-08 14:37:37 +0800

    1. 开始接入登录接口。

--- apps/web-antd/src/api/core/auth.ts
+++ apps/web-antd/src/api/core/auth.ts
@@ -1,51 +1,68 @@
-import { baseRequestClient, requestClient } from '#/api/request';
+import {baseRequestClient, requestClient} from '#/api/request';
 
 export namespace AuthApi {
-  /** 登录接口参数 */
-  export interface LoginParams {
-    password?: string;
-    username?: string;
-  }
+    /** 登录接口参数 */
+    export interface LoginParams {
+        password?: string;
+        username?: string;
+    }
 
-  /** 登录接口返回值 */
-  export interface LoginResult {
-    accessToken: string;
-  }
+    /** 登录接口返回值 */
+    export interface LoginUserInfoVO {
+        userId: number;
+        userType: string;
+        username: string;
+        phone: null | string;
+        roleCodes: string[];
+        coachId: null | number;
+        memberId: null | number;
+        venueIds: number[];
+    }
 
-  export interface RefreshTokenResult {
-    data: string;
-    status: number;
-  }
+    export interface LoginResult {
+        token: string;
+        tokenType: string;
+        userInfo: LoginUserInfoVO;
+    }
+
+
+    export interface RefreshTokenResult {
+        data: string;
+        status: number;
+    }
 }
 
 /**
  * 登录
  */
 export async function loginApi(data: AuthApi.LoginParams) {
-  return requestClient.post<AuthApi.LoginResult>('/auth/login', data);
+    return requestClient.post<AuthApi.LoginResult>('/api/auth/admin/login', data);
 }
 
+export async function getUserInfo() {
+    return requestClient.get<AuthApi.LoginUserInfoVO>('/api/auth/admin/me');
+}
 /**
  * 刷新accessToken
  */
 export async function refreshTokenApi() {
-  return baseRequestClient.post<AuthApi.RefreshTokenResult>('/auth/refresh', {
-    withCredentials: true,
-  });
+    return baseRequestClient.post<AuthApi.RefreshTokenResult>('/auth/refresh', {
+        withCredentials: true,
+    });
 }
 
 /**
  * 退出登录
  */
 export async function logoutApi() {
-  return baseRequestClient.post('/auth/logout', {
-    withCredentials: true,
-  });
+    return baseRequestClient.post('/auth/logout', {
+        withCredentials: true,
+    });
 }
 
 /**
  * 获取用户权限码
  */
 export async function getAccessCodesApi() {
-  return requestClient.get<string[]>('/auth/codes');
+    return requestClient.get<string[]>('/auth/codes');
 }
